bd78ded122c2eee68d4e154901b61cf415a9ba43
geronimoBU <geronimo@bu.edu>
2017-06-29 12:11:53 -0400

    Update madlib2.py

--- madlib2.py
+++ madlib2.py
@@ -2,6 +2,18 @@
 Mad Libs v2
 Created by: Alex Geronimo
 Last Modified: 6/20/17
+
+Items needed: 
+1) Folder labeled as 'resources' inside of the current working directory.
+2) Four .csv files named 'nouns.csv' 'verbs.csv' 'adjectives.csv' 'sentences.csv' inside of 'resources' folder. 
+   Each .csv file contains list of string items. 
+
+Program info: 
+Upon execution, the program will ask for a user name. If the user name is new (.csv file in 'references') a new one will be created.
+User will then be asked to enter a number. That number will be used to select a noun, verb, and adjective and put it into a sentence. 
+Input y to continue or n to exit. 
+Upon exit, the new madlib sentences will be appended to the previously saved madlib game. Unless a new user was createt, it will add 
+the new madlib sentences to the new user .csv file.
 '''
 
 import os
